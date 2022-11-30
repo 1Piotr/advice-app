9148bf35547755aceb3f440861e0545b85d74a62
Piotr Borysewicz <piotrborysewicz@yahoo.co.uk>
2022-11-30 21:02:26 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line react-hooks/rules-of-hooks
+
+
+  
 
 function App() {
+  const [item, setItem] = useState([]); 
+  
+   
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    getAdvice()
+     
+  }, []);
+
+  const  getAdvice =() => {
+    fetch('https://api.adviceslip.com/advice')
+        .then((response) => response.json())
+        .then((data) => {
+           console.log(data);
+           setItem(data.slip);
+        })
+        .catch((err) => {
+           console.log(err.message);
+        });
+   
+
+  }
+  
+
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="box">
+        <div id="container">
+      
+        <p id="num1">Advice #{item.id}<span id="num"></span></p>
+        <p id="ad2">"<span id="advice">{item.advice}"</span></p>
+      
+        <img src="images/pattern-divider-mobile.svg"/>
+        <img src="images/pattern-divider-desktop.svg"/>
+        <div id="img">
+          
+        
+        <input type="image" src="images/icon-dice.svg" alt="" onClick={getAdvice}/>
+      </div>
+      
     </div>
-  );
+    
+    
+        
+    </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
